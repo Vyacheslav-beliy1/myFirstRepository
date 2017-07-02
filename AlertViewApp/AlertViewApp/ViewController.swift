9d982f438c056c5ed602634e7188b44e04e535f2
Vyacheslav-beliy1 <mister.white.2013@gmail.com>
2017-07-02 17:26:00 +0300

    added Alert and ActionSheet;

--- AlertViewApp/AlertViewApp/ViewController.swift
+++ AlertViewApp/AlertViewApp/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - VCLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Actions
+    @IBAction func showAlert(_ sender: UIButton)
+    {
+        let alertViewController = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .alert)
+        
+        let handler = { (action:UIAlertAction) -> Void in
+            print("\(String(describing: action.title))")
+        }
+        
+        let okAction = UIAlertAction(title: "MY - OK", style: .default, handler: handler)
+        alertViewController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "MY - CANCEL", style: .cancel, handler: handler)
+        alertViewController.addAction(cancelAction)
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet(_ sender: UIButton)
+    {
+        let alertViewController = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .actionSheet)
+        
+        let handler = { (action:UIAlertAction) -> Void in
+            print("\(String(describing: action.title))")
+        }
+        
+        let okAction = UIAlertAction(title: "MY - OK", style: .default, handler: handler)
+        alertViewController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "MY - CANCEL", style: .cancel, handler: handler)
+        alertViewController.addAction(cancelAction)
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
 
 }
 

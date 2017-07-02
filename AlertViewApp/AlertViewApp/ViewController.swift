7f96300da6cfcacd64a2c86df60218ea3bd15f81
Vyacheslav-beliy1 <mister.white.2013@gmail.com>
2017-07-02 16:39:07 +0300

    adding single-view project;

---
+++ AlertViewApp/AlertViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertViewApp
+//
+//  Created by bws2007 on 02.07.17.
+//  Copyright © 2017 bws2007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
